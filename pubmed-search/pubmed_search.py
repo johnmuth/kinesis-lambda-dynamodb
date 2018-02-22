4988ccaf4c8b19bcca24aa9ca74b6be50142a3bd
John Muth <johnmuthuk@gmail.com>
2018-02-22 07:40:51 +0000

    correctly doing batch puts to kinesis

--- pubmed-search/pubmed_search.py
+++ pubmed-search/pubmed_search.py
@@ -12,6 +12,12 @@ logging.basicConfig()
 logger = logging.getLogger('pubmed_search')
 logger.setLevel(logging.DEBUG)
 
+kinesis_client = boto3.client('kinesis', region_name='us-east-1')
+stream_name = 'PubmedSearchResults'
+partition_key = '1'
+records_per_put = 500
+results_per_record = 100
+
 def pubmed_search(search):
     logger.info("Executing pubmed search")
     search_results = Entrez.read(Entrez.esearch(db="pubmed", term=search, usehistory="y"), validate=False)
@@ -19,6 +25,30 @@ def pubmed_search(search):
     web_env = search_results["WebEnv"]
     query_key = search_results["QueryKey"]
     logger.debug('Got {} results. web_env={} query_key={})'.format(str(count), web_env, query_key))
+    record_num = 0
+    put_num = 0
+    for start_result_num in range(1, count, records_per_put * results_per_record):
+        put_num += 1
+        end_result_num = min(start_result_num + (records_per_put * results_per_record), count)
+        records = []
+        for first_record_num in range(start_result_num, end_result_num, results_per_record):
+            record_num += 1
+            last_result_num = min((first_record_num + results_per_record - 1), count)
+            logger.debug('record_num={} put_num={} first_record_num={} last_record_num={}'.format(record_num, put_num, first_record_num, last_result_num))
+            records.append({
+                'Data': json.dumps({
+                    'start': first_record_num,
+                    'end': last_result_num,
+                    'web_env': web_env,
+                    'query_key': query_key,
+                }),
+                'PartitionKey': partition_key
+            })
+
+        kinesis_client.put_records(
+            Records=records,
+            StreamName=stream_name
+        )
 
 
 def get_message_attribute(event, attribute):
